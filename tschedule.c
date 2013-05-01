a19486598642b0305fbddaa051770800c9c5f948
Nick Wood <nickswift498@gmail.com>
2013-04-30 19:09:26 -0700

    started off tschedule.c

---
+++ tschedule.c
@@ -0,0 +1,19 @@
+/**
+ * tSchedule
+ *     a simple user-space thread scheduler
+ *     programming assingment 2 for CS111 @ UCSC
+ *     Spring 2013
+ *
+ *     Authors: Konstantin Litovsky, Tyler Esser, Andrew Bao, and Nick Wood
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Main application starting point */
+int main(void){
+	
+	/* STUB */
+
+}
+ 
