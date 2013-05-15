105234ed6298a78da7ef3ca91c4cf054be7cc5a8
Konstantin <konstantin.litov@gmail.com>
2013-05-14 21:50:28 -0700

    Added Standard Comment Intro

--- scheduler.h
+++ scheduler.h
@@ -1,4 +1,9 @@
 /**
+ * Modified: scheduler.h
+ * PROGRAMMING ASSIGNMENT 2
+ * CMPS 111 Spring 2013
+ * Authors: Andrew Bao, Konstantin Litovskiy, Tyler Esser & Nick Wood
+ * 
  * Scheduler.h
  */
  
