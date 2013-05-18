ae7021b44bf2c07e870d49d983d3e5a6cce754f1
wormingdead <wormingdead@gmail.com>
2013-05-18 15:26:16 -0700

    Formatting.

--- scheduler.c
+++ scheduler.c
@@ -3,39 +3,35 @@
  * PROGRAMMING ASSIGNMENT 2
  * CMPS 111 Spring 2013
  * Authors: Andrew Bao, Konstantin Litovskiy, Tyler Esser & Nick Wood
- * 
+ *
  * Scheduler.c
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 #include "scheduler.h"
-#include "thread_list.c"
-#include "thread_object.h"
+#include "thread_list.h"
+#include "thread_obj.h"
 
 typedef struct SchedLottery{
-	
-	/* 
-	 */
-	TLRef * threadList;
-	TLNodeRef * runningNode;
+	/**/
+	TLRef *threadList;
+	TLNodeRef *runningNode;
 	int time_quantum;
-			
 } SchedLottery;
 
 /* Constructor */
 SLotto newLottery(int initQuantum){
 	/* Allocate memory for the new node */
 	SLotto _lotto = malloc(sizeof(SchedLottery));
-	
+
 	/* Seed randomizer */
 	srand (time(NULL));
-	
+
 	/* Set loto data values */
 	_loto->threadList 	= newThreadList();
-	_loto->runningNode	= NULL;
+	_loto->runningNode 	= NULL;
 	_loto->time_quantum = initQuantum;
 	_loto->winner 		= -1;
 	setupTimer(_lotto->time_quantum);
@@ -43,10 +39,10 @@ SLotto newLottery(int initQuantum){
 	return(_loto);
 }
 
-void freeSchedLottery(SLotto * oldLotto){
-	freeThreadListNode( oldLotto->runningNode );
-	freeThreadList( oldLotto->threadList );
-	
+void freeSchedLottery(SLotto *oldLotto){
+	freeThreadListNode(oldLotto->runningNode);
+	freeThreadList(oldLotto->threadList);
+
 	free(*oldLotto);
 	oldLotto = NULL;
 }
@@ -60,18 +56,16 @@ void runScheduler(SLotto _lotto){
 				run = false;
 			}
 			startTimer();
-			/*startThread()*/
 		}
 	}
 }
 
-TLNodeRef getNextNode(SLotto * _lotto){
-
-	TLRef * _list = _lotto->threadList;
+TLNodeRef getNextNode(SLotto *_lotto){
+	TLRef *_list = _lotto->threadList;
 	int newIndex = rand() % _list->ticketCount + 1;
-			
-	TLNodeRef * _node = getNodeAtIndex (_list, newIndex);
-	
+
+	TLNodeRef *_node = getNodeAtIndex(_list, newIndex);
+
 	return _node;
 }
 
@@ -86,6 +80,4 @@ void stopTimer(SLotto * _lotto){
 void startTimer(SLotto * _lotto){
 
 }
-
-
 
--- scheduler.h
+++ scheduler.h
@@ -3,27 +3,29 @@
  * PROGRAMMING ASSIGNMENT 2
  * CMPS 111 Spring 2013
  * Authors: Andrew Bao, Konstantin Litovskiy, Tyler Esser & Nick Wood
- * 
+ *
  * Scheduler.h
  */
 
-#if !defined(_SCHEDULER_H_INCLUDE_)
+#ifndef _SCHEDULER_H_INCLUDE
 #define _SCHEDULER_H_INCLUDE
-#endif 
 
-typedef struct SchedLottery * SLotto;
+typedef struct SchedLottery *SLotto;
 
 /* Constructor */
 SLotto newLottery(int initQuantum);
 
 /* Cleanup */
 void runScheduler(SLotto Lotto);
-void freeSchedLottery(SLotto * oldLotto);
+void freeSchedLottery(SLotto *oldLotto);
 
 /* Scheduler Functions */
-TLNodeRef getNextNode(SLotto * _lotto);
+TLNodeRef getNextNode(SLotto *_lotto);
 
 /* Timer Manipulations */
 void setupTimer(int timerQuantum);
-void stopTimer(SLotto * _lotto);
-void startTimer(SLotto * _lotto);
+void stopTimer(SLotto *_lotto);
+void startTimer(SLotto *_lotto);
+
+#endif
+
