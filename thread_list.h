f0885d9f6c49657ee65a7060b26348a471b586b9
wormingdead <wormingdead@gmail.com>
2013-05-18 12:36:18 -0700

    Correct the preprocessor define. Also whitespace.

--- thread_list.h
+++ thread_list.h
@@ -3,28 +3,27 @@
  * PROGRAMMING ASSIGNMENT 2
  * CMPS 111 Spring 2013
  * Authors: Andrew Bao, Konstantin Litovskiy, Tyler Esser & Nick Wood
- * 
+ *
  * thread_list.h: Thread List
  * A linked list containing the number of lottery tickets,
  * as well as identifying information regarding the threads
  * being kept track of.
  */
 
-#if !defined(_THREADLIST_H_INCLUDE_)
+#ifndef _THREADLIST_H_INCLUDE
 #define _THREADLIST_H_INCLUDE
-#endif
 
 /* Exported Types */
-typedef struct ThreadList 		* TLRef;
-typedef struct ThreadListNode 	* TLNodeRef;
+typedef struct ThreadList *TLRef;
+typedef struct ThreadListNode *TLNodeRef;
 
 /* Constructors */
 TLRef newThreadList(void);
 TLNodeRef newThreadListNode(int initTickets, ThreadObject newThread);
 
 /* Destructors */
-void freeThreadList(TLRef * pL);
-void freeThreadListNode(TLNodeRef * pN);
+void freeThreadList(TLRef *pL);
+void freeThreadListNode(TLNodeRef *pN);
 
 /* Accessors */
 TLNodeRef getFront(TLRef L);
@@ -37,5 +36,6 @@ void removeThreadNode(TLRef L, int _threadID);
 TLNodeRef popThread(TLRef L);
 
 /* List Operations */
-
 void printList(TLRef L);
+
+#endif
